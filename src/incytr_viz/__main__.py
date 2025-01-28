31e45287d770388425d565f7343590d9b4352e6e
Ian Cossentino <ian.cossentino@cellsignal.com>
2025-01-28 14:47:43 -0500

    remove need for subprocess override hup signal handling to avoid reload

--- src/incytr_viz/__main__.py
+++ src/incytr_viz/__main__.py
@@ -1,8 +1,50 @@
 import argparse
-import subprocess
+import sys
+from incytr_viz.app import create_app
 from incytr_viz.util import create_logger
 
 logger = create_logger(__name__)
+
+
+import gunicorn.app.base
+from gunicorn.arbiter import Arbiter
+
+
+class CustomArbiter(Arbiter):
+
+    def handle_hup(self):
+        self.handle_term()
+
+
+class StandaloneApplication(gunicorn.app.base.BaseApplication):
+
+    def __init__(self, app, options=None):
+        self.options = options or {}
+        self.application = app
+        super().__init__()
+
+    def load_config(self):
+        config = {
+            key: value
+            for key, value in self.options.items()
+            if key in self.cfg.settings and value is not None
+        }
+        for key, value in config.items():
+            self.cfg.set(key.lower(), value)
+
+    def load(self):
+        return self.application
+
+    def run(self):
+        logger.info(
+            f"Running incytr-viz with gunicorn at {self.cfg.settings['bind'].value}"
+        )
+        try:
+            CustomArbiter(self).run()
+        except RuntimeError as e:
+            print("\nError: %s\n" % e, file=sys.stderr)
+            sys.stderr.flush()
+            sys.exit(1)
 
 
 def main():
@@ -23,13 +65,8 @@ def main():
     CLUSTERS = args.clusters
 
     logger.info("Running Incytr Viz using gunicorn web server")
-    logger.info("Serving at http://localhost:8000")
-    subprocess.run(
-        [
-            "gunicorn",
-            f"incytr_viz.app:create_app(pathways_file='{PATHWAYS}', clusters_file='{CLUSTERS}')",
-        ]
-    )
+    app = create_app(pathways_file=PATHWAYS, clusters_file=CLUSTERS)
+    StandaloneApplication(app=app).run()
 
 
 if __name__ == "__main__":
